569a33ac9ead1b613720ae9ad81ec5bee46fe296
Tianle Wang <wtl233wtl@gmail.com>
2024-01-16 16:29:25 -0800

    fix

--- sample.py
+++ sample.py
@@ -3,6 +3,7 @@ from models import model_dict
 from torch.utils.data import Dataset
 import argparse
 import pickle
+import torch
 
 
 class ImgDataset(Dataset):
@@ -16,14 +17,22 @@ class ImgDataset(Dataset):
         img, label = self.data[idx]
         return img, label, idx
 
+def get_name(model_path):
+    """parse teacher name"""
+    segments = model_path.split('/')[-2].split('_')
+    if segments[0] != 'wrn':
+        return segments[0]
+    else:
+        return segments[0] + '_' + segments[1] + '_' + segments[2]
 
-def load_model(path):
-    # TODO
-    # path should be model name
-    # see usage in models/_init_.py
-    model = model_dict[path](num_classes=100)
+
+def load(model_path, n_cls):
+    print('==> loading teacher model')
+    model_t = get_name(model_path)
+    model = model_dict[model_t](num_classes=n_cls)
+    model.load_state_dict(torch.load(model_path)['model'])
+    print('==> done')
     return model
-    ...
 
 
 def store_data(data, output_path):
@@ -35,9 +44,9 @@ def store_data(data, output_path):
 def main(args):
     # TODO: first load teacher and student from the path
     # you can first try to solve one t/s pair like (t: wrn-40-2, s: wrn-16-2)
-    t_model = load_model(args.t_model)
+    t_model = load(args.t_model, args.class_num)
     t_model = t_model.to('cuda')
-    s_model = load_model(args.s_model)
+    s_model = load(args.s_model, args.class_num)
     s_model = s_model.to('cuda')
 
     # call sampler to generate data
@@ -51,9 +60,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='data sampling')
-    parser.add_argument('--t_model', default='...',
+    parser.add_argument('--path_t', default='...',
                         help='teacher model path')
-    parser.add_argument('--s_model', default='...',
+    parser.add_argument('--path_s', default='...',
                         help='student model path')
     parser.add_argument('--output_path', default='add_data/cifar-100/train_dataset_add.pkl',
                         help='generated data path')
